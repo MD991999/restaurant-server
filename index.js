3d955b0f584647ac4bb1b3db649d7c9bd455ec1a
Michelle Devassia <michelledevassia1999@gmail.com>
2023-04-27 16:24:39 +0530

    restaurant list json server

---
+++ index.js
@@ -0,0 +1,19 @@
+// import json server
+const jsonserver=require('json-server')
+// create json-server app
+const server=jsonserver.create()
+// set up path for db.json
+const router=jsonserver.router("db.json")
+// returns middleware used by json server
+const middleware=jsonserver.defaults()
+// set up port
+const port=process.env.PORT || 4000
+// use router middleware in server
+server.use(middleware)
+server.use(router)
+
+// app listen
+server.listen(port,()=>{
+    console.log('json server started at port number 4000');
+})
+
